c9bbc0c2df4490b575f1c22683175563ed510f11
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-04-25 00:57:42 +0200

    ajout de hashcode et equals dans entity

--- src/main/java/app/back/dto/AbstractEntity.java
+++ src/main/java/app/back/dto/AbstractEntity.java
@@ -2,6 +2,8 @@ package app.back.dto;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @MappedSuperclass
 public abstract class AbstractEntity<T extends AbstractEntity<T>> implements Identifiable, Mixin<T> {
 
@@ -23,6 +25,18 @@ public abstract class AbstractEntity<T extends AbstractEntity<T>> implements Ide
         if(this.id != null) {
             dbEntity.setId(id);
         }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if(object == null || getClass() != object.getClass()) return false;
+        AbstractEntity<?> that = (AbstractEntity<?>) object;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
 
