c3e5aa95f37722b2f181d465b44e30153a31701d
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-04-27 03:01:32 +0200

    meilleur gestion des champs lors de l'update d'un event

--- src/main/java/app/back/service/DtoEventService.java
+++ src/main/java/app/back/service/DtoEventService.java
@@ -1,18 +1,52 @@
 package app.back.service;
 
 import app.back.dto.Event;
+import app.back.exception.BackBadRequestException;
 import app.back.repository.EventRepository;
 import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class DtoEventService extends DtoAbstractEntityService<Event, @NonNull EventRepository> {
 
     protected DtoEventService(@NonNull EventRepository repository) {
         super(repository);
+    }
+
+    @EntityGraph(attributePaths = {
+            "subEvents",
+            "participants",
+            "todoListEntries"
+    })
+    @Override
+    public Optional<Event> findById(long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Event save(Event entity) {
+        if(entity.getEventName() == null || entity.getEventName().isBlank()) {
+            throw new BackBadRequestException("L'événement doit obligatoirement avoir un nom.");
+        }
+        if(entity.getParentEvent() == null) {
+            var result = this.findByEventName(entity.getEventName());
+            if(result.isPresent() && !Objects.equals(result.get().getId(), entity.getId())) {
+                throw new BackBadRequestException("L'événement " + entity.getEventName() + " est déjà existant.");
+            }
+        } else {
+            var result = this.findByEventName(entity.getParentEvent().getId(), entity.getEventName());
+            if(result.isPresent() && !Objects.equals(result.get().getId(), entity.getId())) {
+                throw new BackBadRequestException("Le sous événement " + entity.getEventName() + " est déjà existant.");
+            }
+        }
+
+        return super.save(entity);
     }
 
     @Override
@@ -22,7 +56,9 @@ public class DtoEventService extends DtoAbstractEntityService<Event, @NonNull Ev
         dbEntity.setTricountUrl(entityToSave.getTricountUrl());
         dbEntity.setStartDate(entityToSave.getStartDate());
         dbEntity.setEndDate(entityToSave.getEndDate());
-        dbEntity.setParentEvent(entityToSave.getParentEvent());
+        if(entityToSave.shouldUpdateParentEvent()) {
+            dbEntity.setParentEvent(entityToSave.getParentEvent());
+        }
         if(entityToSave.shouldUpdateSubEvents()) {
             dbEntity.setSubEvents(entityToSave.getSubEvents());
         }
