611dc72f8128c2fb7d2e500a5993841172c52fdc
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-04-28 02:03:42 +0200

    ajout @Service

--- src/main/java/app/back/service/DtoTodoEntryService.java
+++ src/main/java/app/back/service/DtoTodoEntryService.java
@@ -3,7 +3,9 @@ package app.back.service;
 import app.back.dto.TodoEntry;
 import app.back.repository.TodoEntryRepository;
 import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DtoTodoEntryService extends DtoAbstractEntityService<TodoEntry, @NonNull TodoEntryRepository> {
     protected DtoTodoEntryService(@NonNull TodoEntryRepository repository) {
         super(repository);
