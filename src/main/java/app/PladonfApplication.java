962ee3449e0b279ba5d206787c0df8f78562cd80
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-05-03 01:53:30 +0200

    remove public inutile

--- src/main/java/app/PladonfApplication.java
+++ src/main/java/app/PladonfApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PladonfApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(PladonfApplication.class, args);
 	}
 
