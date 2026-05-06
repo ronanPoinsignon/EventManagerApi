afba9b7f97759f6a44785be9c79ca749542c9944
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-05-06 19:20:02 +0200

    ajout de plus de rôles au client back

--- init-keycloak.sh
+++ init-keycloak.sh
@@ -2,7 +2,7 @@
 set -e
 
 until /opt/keycloak/bin/kcadm.sh config credentials \
-  --server http://keycloak:8080 \
+  --server http://keycloak:"$KEYCLOAK_PORT" \
   --realm master \
   --user "$KEYCLOAK_USER" \
   --password "$KEYCLOAK_PASSWORD"; do
@@ -14,9 +14,16 @@ done
 echo "Ajout des rôles au user"
 
 /opt/keycloak/bin/kcadm.sh add-roles \
-  -r event_organizer \
-  --uusername service-account-pladonf_back_end \
+  -r "$KEYCLOAK_REALM" \
+  --uusername service-account-"$KEYCLOAK_CLIENT_ID"\
   --cclientid realm-management \
-  --rolename view-users
+  --rolename view-users \
+  --rolename query-users \
+  --rolename manage-users \
+  --rolename view-realm \
+  --rolename create-client \
+  --rolename manage-clients \
+  --rolename query-clients \
+  --rolename view-clients \
 
 echo "Fin de traitement"
