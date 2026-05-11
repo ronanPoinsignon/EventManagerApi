1deb4cccdc2339ca529a48cddcf423084803ec6f
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-05-11 22:28:36 +0200

    ajout nouvelle exception

---
+++ src/main/java/app/back/exception/BackResourceAccessException.java
@@ -0,0 +1,9 @@
+package app.back.exception;
+
+public class BackResourceAccessException extends BackException {
+
+    public BackResourceAccessException(String message) {
+        super(message);
+    }
+
+}
