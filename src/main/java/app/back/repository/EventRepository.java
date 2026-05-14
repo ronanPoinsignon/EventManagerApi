4dc6377a61df2f988839ffa4dc9c6dde09392d36
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-05-14 21:34:11 +0200

    récupération des event uniquement parents

--- src/main/java/app/back/repository/EventRepository.java
+++ src/main/java/app/back/repository/EventRepository.java
@@ -17,7 +17,7 @@ public interface EventRepository extends AbstractEntityRepository<Event> {
     @NativeQuery("select * from events where event_name = ?2 and parent_event_id = ?1")
     Optional<Event> findByEventName(long parentId, String eventName);
 
-    @NativeQuery("select * from events where (end_date is null AND start_date >= ?1) OR end_date >= ?1")
+    @NativeQuery("select * from events where ((end_date is null AND start_date >= ?1) OR end_date >= ?1) and parent_event_id is null")
     List<Event> findAllBeforeEnd(LocalDateTime date);
 
     @NativeQuery("select * from events where parent_event_id is null order by creation_date desc limit 1")
