a307a17e633951de2beac3c8cb00de319b5d1b74
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-06-10 19:35:42 +0200

    gestion des notifications back

--- src/main/java/app/configuration/ScheduleNotificationConfiguration.java
+++ src/main/java/app/configuration/ScheduleNotificationConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableScheduling
@@ -26,8 +28,50 @@ public class ScheduleNotificationConfiguration {
     @Scheduled(fixedRate = 60000)
     @Transactional
     public void notifyEvents() {
-        var notifications = scheduleNotificationService.findNotifications(Instant.now(), 1);
-        notifications.forEach(this::sendToRabbit);
+        var notifications30D = scheduleNotificationService.findNotifications(Instant.now(), 30);
+        var notifications7D = scheduleNotificationService.findNotifications(Instant.now(), 7);
+        var notifications1D = scheduleNotificationService.findNotifications(Instant.now(), 1);
+
+        var entityId1DSet = notifications1D.stream().map(notification -> notification.getEntity().getId()).collect(Collectors.toSet());
+        var entityId7DSet = notifications7D.stream().map(notification -> notification.getEntity().getId()).collect(Collectors.toSet());
+
+        var iterator7d = notifications7D.iterator();
+        while(iterator7d.hasNext()) {
+            var notification = iterator7d.next();
+            var entityId = notification.getEntity().getId();
+            if(!entityId1DSet.contains(entityId)) {
+                continue;
+            }
+
+            notification.setNotified(true);
+            scheduleNotificationService.save(notification);
+            iterator7d.remove();
+        }
+
+        var iterator30d = notifications30D.iterator();
+        while(iterator30d.hasNext()) {
+            var notification = iterator30d.next();
+            var entityId = notification.getEntity().getId();
+            if(!entityId1DSet.contains(entityId) || entityId7DSet.contains(entityId)) {
+                continue;
+            }
+
+            notification.setNotified(true);
+            scheduleNotificationService.save(notification);
+            iterator30d.remove();
+        }
+
+        this.notify(notifications30D);
+        this.notify(notifications7D);
+        this.notify(notifications1D);
+    }
+
+    private void notify(Collection<ScheduleNotification> notifications) {
+        notifications.forEach(notification -> {
+            this.sendToRabbit(notification);
+            notification.setNotified(true);
+            scheduleNotificationService.save(notification);
+        });
     }
 
     private void sendToRabbit(ScheduleNotification notification) {
